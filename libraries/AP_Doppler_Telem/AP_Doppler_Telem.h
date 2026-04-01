388f20fbd021270500ed1e97c1e6f7372f29879f
Antipulsar <3503277527@qq.com>
2026-04-01 22:20:33 +0800

    add AP_Doppler_FitSurface.cpp & .h

--- libraries/AP_Doppler_Telem/AP_Doppler_Telem.h
+++ libraries/AP_Doppler_Telem/AP_Doppler_Telem.h
@@ -35,6 +35,13 @@ public:
     void update();
     void send();
     bool get_velocity_body(Vector3f &vel_body_mps, uint32_t &t_ms, float &quality, DVL_LockState &lock) const;
+    bool get_bi_msg(DVL_BI_Msg &msg) const;
+    bool get_bd_msg(DVL_BD_Msg &msg) const;
+    bool get_wi_msg(DVL_WI_Msg &msg) const;
+    bool get_ua_msg(DVL_U_Msg &msg) const;
+    bool get_ub_msg(DVL_U_Msg &msg) const;
+    bool get_uc_msg(DVL_U_Msg &msg) const;
+    bool get_ud_msg(DVL_U_Msg &msg) const;
     const AP_Doppler_Parameters &parameters() const { return *_doppler_parameters; }
 
     static AP_Doppler_Telem *get_singleton(void) {
@@ -45,10 +52,20 @@ protected:
     
 
 private:
+    void update_simulated_messages();
 
     AP_Doppler_Parameters *_doppler_parameters;
     AP_HAL::UARTDriver *port;
     AP_Doppler_Backend *_backend;
+    mutable HAL_Semaphore _sim_sem;
+    uint32_t _sim_last_update_ms = 0;
+    DVL_BI_Msg _sim_bi_msg {};
+    DVL_BD_Msg _sim_bd_msg {};
+    DVL_WI_Msg _sim_wi_msg {};
+    DVL_U_Msg _sim_ua_msg {};
+    DVL_U_Msg _sim_ub_msg {};
+    DVL_U_Msg _sim_uc_msg {};
+    DVL_U_Msg _sim_ud_msg {};
     static AP_Doppler_Telem *singleton;
 
 };
